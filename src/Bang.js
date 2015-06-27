6128300756db350963aadcdad72d328f6c8f9165
lo-th <3dflashlo@gmail.com>
2015-06-27 19:07:37 +0200

    optimize

--- src/Bang.js
+++ src/Bang.js
@@ -1,75 +1,78 @@
 NEO.Bang = function(obj){
 
     this.type = 'bang';
-    this.keys = [];
-    this.list = [];
+    this.items = [];
+    this.keys = obj.keys || [];
     
     NEO.Proto.call( this, obj );
 
     // click
-    this.f[2] = function(e){ this.add(e); }.bind(this);
+    this.f[2] = function(e){ this.addMouse(e); }.bind(this);
     this.c[6].onclick = this.f[2];
 
     this.init();
 
+    if(this.keys.length) this.addKeys();
 }
 
 NEO.Bang.prototype = Object.create( NEO.Proto.prototype );
 NEO.Bang.prototype.constructor = NEO.Bang;
 
-NEO.Bang.prototype.add = function(e){
-
-    var f = NEO.main.getFrameClick(e.clientX);
-
-    if (this.list.indexOf(f) > -1) {
-        this.remove(this.list.indexOf(f));
-        return;
+NEO.Bang.prototype.addKeys = function(){
+    var i = this.keys.length, k, f;
+    while(i--){
+        f = this.keys[i];
+        this.add(f);
     }
-
-    var key = NEO.main.keybox(f);
-    key.name = f;
-
-    this.c[5].appendChild(key);
-    this.keys.push(key);
     this.sort();
+}
 
+NEO.Bang.prototype.addMouse = function(e){
+    var f = NEO.main.getFrameClick(e.clientX);
+    if (this.keys.indexOf(f) > -1) {
+        this.remove(this.keys.indexOf(f));
+    } else {
+        this.add(f);
+        this.sort();
+    }
+};
+
+NEO.Bang.prototype.add = function(f){
+    var k = NEO.main.keybox(f);
+    this.c[5].appendChild(k);
+    k.name = f;
+    this.items.push(k);
 };
 
 NEO.Bang.prototype.remove = function(i){
-
-    this.c[5].removeChild( this.keys[i]);
-    this.keys.splice( i, 1 );
+    this.c[5].removeChild( this.items[i]);
+    this.items.splice( i, 1 );
     this.sort();
-
 };
 
 NEO.Bang.prototype.sort = function(){
-
-    this.keys.sort( function ( a, b ) { return a.name - b.name; } );
-    this.list = [];
-    var i = this.keys.length;
-    while(i--) this.list.unshift(this.keys[i].name);
-
+    this.items.sort( function ( a, b ) { return a.name - b.name; } );
+    this.keys = [];
+    var i = this.items.length;
+    while(i--) this.keys.unshift(this.items[i].name);
 };
 
 NEO.Bang.prototype.update = function(f){
-
     var active = false;
-    if (this.list.indexOf(f) > -1) active = true;
+    if (this.keys.indexOf(f) > -1) active = true;
 
     if(active) this.c[5].style.background = 'rgba(86,175,178,0.3)';
     else this.c[5].style.background = 'none';
 
     this.callback(active);
-
 };
 
 NEO.Bang.prototype.setSize = function(){
     this.c[5].style.width = NEO.main.maxSize+'px';
     var w = NEO.main.frameSize;
-    var i = this.keys.length, k;
+    var i = this.items.length, k;
     while(i--){
-        k = this.keys[i];
+        k = this.items[i];
         k.style.width = w + 'px';
         k.style.left = (k.name*w) + 'px';
 
