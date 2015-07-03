a0177a313d527f93387c70568cd7e67a7c1389be
lo-th <3dflashlo@gmail.com>
2015-07-03 08:21:36 +0200

    up

--- _compile/build.bat
+++
@@ -1,2 +0,0 @@
-python build.py --include neo --output ../build/neo.js
-python build.py --include neo --minify --output ../build/neo.min.js
--- src/Plus.js
+++
@@ -1,1 +0,0 @@
-var performance, define, exports, module;
